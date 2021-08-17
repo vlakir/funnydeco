cee7748e385f802076966157d7ebed6f6bd7422b
vlakir <vek2006_73@mail.ru>
2021-08-17 14:24:58 +0300

    + examples

---
+++ examples/benchmark_example.py
@@ -0,0 +1,20 @@
+from core import benchmark
+
+
+# noinspection PyUnusedLocal
+@benchmark
+def long_loop(print_benchmark=False, benchmark_name='') -> int:
+    """
+    Demo class for example of benchmark
+    """
+    result = 0
+    for i in range(int(1e7)):
+        result += i
+    return result
+
+
+if __name__ == '__main__':
+    print('With benchmark:')
+    long_loop(print_benchmark=True, benchmark_name='Long loop procedure')
+    print('Without benchmark:')
+    long_loop()
---
+++ examples/singleton_example.py
@@ -0,0 +1,38 @@
+from core import singleton, SingletonReseter
+from time import sleep
+
+
+@singleton
+class SlowInitClass(SingletonReseter):
+    """
+    Demo class for example of singleton decorator
+    """
+    def __init__(self):
+        self.variable = 100
+        print("I'm sleeping...")
+        sleep(5)
+        print("I'v woken up")
+
+
+if __name__ == '__main__':
+    print('First dumbass init:')
+    dumbass1 = SlowInitClass()
+    print(dumbass1.variable)
+    print()
+
+    print('Second dumbass init:')
+    dumbass2 = SlowInitClass()
+    print(dumbass2.variable)
+    print()
+
+    print('Third dumbass init:')
+    print('Temporary stopping singleton behaviour')
+    SlowInitClass().reset_singleton()
+    dumbass3 = SlowInitClass()
+    print(dumbass2.variable)
+    print()
+
+    print('Fourth dumbass init:')
+    dumbass4 = SlowInitClass()
+    print(dumbass4.variable)
+    print()
